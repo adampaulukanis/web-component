2f32e0d28369c561599b9e623e4ba8abce839652
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-06-17 23:33:41 +0100

    Customizing interactivity with HTML attributes

--- main.js
+++ main.js
@@ -14,11 +14,29 @@ class GreetingMessage extends HTMLElement {
     }
 
     connectedCallback(){
-        console.log('Connected', this);
+        let btn = this.querySelector('button');
+        if (!btn) return;
+        btn.addEventListener('click', this.clickHandler);
     }
 
     disconnectedCallback(){
-        console.log('Disconnected', this);
+        let btn = this.querySelector('button');
+        if (!btn) return;
+        btn.removeEventListener('click', this.clickHandler);
+    }
+
+    clickHandler(event) {
+            let host = event.target.closest('greeting-message');
+
+            let target = host.querySelector('.message');
+            if (!target) return;
+
+            let name = host.getAttribute('name');
+            target.textContent = `Hello there, ${name ? name : 'friend'}! How is your day? ;^)`;
+
+            setTimeout(() => {
+                target.textContent = '';
+            }, 5000);
     }
 };
 
